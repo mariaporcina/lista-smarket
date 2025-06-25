e33f119324d7d4f6b485c2d6c87e04ccc31225a8
Maria Porcina <43049787+mariaporcina@users.noreply.github.com>
2025-06-25 20:44:31 -0300

    refactor: refactoring user modules (#1)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,9 @@ import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
-import { AuthMiddleware } from './auth/auth.middleware';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { UserProfileModule } from './user-profile/user-profile.module';
 import { ShoppingListProductModule } from './shopping-list-product/shopping-list-product.module';
 
 @Module({
@@ -19,7 +17,6 @@ import { ShoppingListProductModule } from './shopping-list-product/shopping-list
     PrismaModule,
     AuthModule,
     ShoppingListModule,
-    UserProfileModule,
     ShoppingListProductModule,
   ],
   controllers: [AppController],
